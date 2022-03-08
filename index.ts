2d98425b2abcbb5b1531958ff59ca8677e9201c8
John Richardson <jkrichardson08@gmail.com>
2022-03-08 07:35:21 -0600

    k

--- index.ts
+++
@@ -1,6 +0,0 @@
-import { outputStringDay1 } from "./solutions/day1";
-import { outputStringDay2 } from "./solutions/day2";
-import { outputStringDay3 } from "./solutions/day3";
-import { outputStringDay4 } from "./solutions/day4"
-
-console.log(outputStringDay1, outputStringDay2, outputStringDay3, outputStringDay4)
---
+++ solutions/index.ts
@@ -0,0 +1,11 @@
+import { outputStringDay1 } from "./day1";
+import { outputStringDay2 } from "./day2";
+import { outputStringDay3 } from "./day3";
+import { outputStringDay4 } from "./day4";
+
+console.log(
+    outputStringDay1,
+    outputStringDay2,
+    outputStringDay3,
+    outputStringDay4
+);
